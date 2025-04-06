8a4e01e5e9898ffd3c29075d86873c1a516aa191
srafaelnovoa <rafael.novoa@gmail.com>
2025-04-05 21:05:03 -0400

    Update init pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,5 @@
+function About() {
+  return <h1 className="text-2xl font-bold">About Me</h1>;
+}
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,5 @@
+function Contact() {
+  return <h1 className="text-2xl font-bold">Contact Me</h1>;
+}
+
+export default Contact;
