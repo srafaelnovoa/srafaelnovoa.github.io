935baa6d0ffb536d4cfa6846a764173b153821e3
srafaelnovoa <rafael.novoa@gmail.com>
2025-04-05 20:49:18 -0400

    Replace old site with new React portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white p-4 flex gap-6">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+function Home() {
+  return <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>;
+}
+export default Home;
